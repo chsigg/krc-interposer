495fcf5c27e22ae5f777168cbcca2e6f13fd3250
Christian Sigg <csigg@gmx.ch>
2026-03-08 21:04:49 +0100

    Lower trigger threshold and check boil setting during setup()

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,7 @@ private:
   ArduinoAnalogReadPin ref_pin_{kDialRefPin};
 };
 
+static void shutdown();
 static void poweroff();
 
 // --- Hardware Instantiation ---
@@ -68,6 +69,7 @@ StoveDial dial(dial_read_pin, throttle_config);
 ArduinoBuzzer buzzer(NRF_PWM3, kBuzzerPPin, kBuzzerNPin);
 Beeper beeper(buzzer);
 ArduinoAnalogWritePin red_led(LED_RED);
+ArduinoDigitalWritePin green_led(LED_GREEN);
 
 // Logic Modules
 TrendAnalyzer analyzer;
@@ -82,15 +84,11 @@ BleTelemetry telemetry(bleuart, controller, analyzer);
 StoveConfig stove_config;
 StoveSupervisor supervisor(dial, actuator, controller, beeper, analyzer,
                            bypass_pin, stove_config,
-                           throttle_config, poweroff);
+                           throttle_config, shutdown);
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial && millis() < 1000) {
-    delay(10);
-  }
-
-  Log << "KRC Interceptor Starting...\n";
+  bypass_pin.begin();
+  bypass_pin.set(PinState::Low);
 
   for (int pin : {D5, D6, D7, D8, D9}) {
     pinMode(pin, INPUT_PULLDOWN);
@@ -99,23 +97,38 @@ void setup() {
   analogReadResolution(ADC_RESOLUTION);
   analogWriteResolution(ADC_RESOLUTION);
 
-  bypass_pin.begin();
-  bypass_pin.set(PinState::Low);
-
   dial_read_pin.begin();
-  buzzer.begin();
   stove_pwm.begin();
   red_led.begin();
+  green_led.begin();
+  green_led.set(PinState::Low);
 
-  beeper.beep(Beeper::Signal::POWER_ON);
+  Serial.begin(115200);
 
   Bluefruit.begin(1, 1);
   Bluefruit.autoConnLed(false);
   Bluefruit.setTxPower(8);
   Bluefruit.setName("KRC Interposer");
 
+  while (dial_read_pin.read() < throttle_config.boil) {
+    if (millis() > 10000) {
+      Log << "Boil level not detected, powering off.\n";
+      poweroff();
+    }
+  }
+
+  buzzer.begin();
+  beeper.beep(Beeper::Signal::POWER_ON);
+
+  while (!Serial && millis() < 1000) {
+    beeper.update();
+    delay(10);
+  }
+  Log << "KRC Interceptor Starting...\n";
+
   thermometer.begin();
   telemetry.begin();
+  green_led.set(PinState::High);
 }
 
 void loop() {
@@ -126,7 +139,7 @@ void loop() {
   delay(20);
 }
 
-static void poweroff() {
+static void shutdown() {
   beeper.beep(Beeper::Signal::POWER_OFF);
   while (!beeper.isIdle()) {
     delay(10);
@@ -136,11 +149,15 @@ static void poweroff() {
   Log << "Powering off...\n";
 
   thermometer.end();
-
-  Serial.flush();
   telemetry.update();
-  Serial.end();
   telemetry.end();
+
+  poweroff();
+}
+
+static void poweroff() {
+  Serial.flush();
+  Serial.end();
 
   for (auto pwm : {NRF_PWM0, NRF_PWM1, NRF_PWM2, NRF_PWM3}) {
     nrf_pwm_disable(pwm);
@@ -158,7 +175,7 @@ static void poweroff() {
   // Set up boot trigger when pin is 7/8 of VDD.
   nrf_lpcomp_disable(NRF_LPCOMP);
   const nrf_lpcomp_config_t lpcomp_config = {.reference =
-                                                 NRF_LPCOMP_REF_SUPPLY_7_8,
+                                                 NRF_LPCOMP_REF_SUPPLY_6_8,
                                              .detection = NRF_LPCOMP_DETECT_UP,
                                              .hyst = NRF_LPCOMP_HYST_ENABLED};
   nrf_lpcomp_configure(NRF_LPCOMP, &lpcomp_config);
